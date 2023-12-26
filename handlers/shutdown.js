0142b4ee5740423d29eede83eca7f23d24696aff
Александр Мигаль <magalalecs2003@gmail.com>
2023-12-26 22:14:18 +0200

    shutdown fix-2

--- handlers/shutdown.js
+++ handlers/shutdown.js
@@ -3,12 +3,10 @@ import express from 'express';
 const shutdown = express.Router();
 
 shutdown.route('/')
-app.post('/shutdown', (req, res) => {
     console.log('Закриття сервера по запросу...');
     res.send('Сервер закривається по запиту...');
     server.close(() => {
     console.log('Сервер закритий.');
     });
-});
 
 export default shutdown;
